2ada1f61e17d07c2f08ffc661547b3092127e592
BuildTools <unconfigured@null.spigotmc.org>
2020-09-04 19:07:48 +0700

    Main and Handlers

--- src/org/Skyblueplayer/handler/message.java
+++ src/org/Skyblueplayer/handler/message.java
@@ -1,9 +1,14 @@
 package org.Skyblueplayer.handler;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 public class message {
     public void consolelog(String Message) {
         Bukkit.getConsoleSender().sendMessage(Message);
+    }
+
+    public void playermessage(Player player, String Message) {
+        player.sendMessage(Message);
     }
 }
